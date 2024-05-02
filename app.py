adfbf3e6f0993d7a83917269a565b8bf7c52b41b
laura carrera <lauradcarrera44@gmail.com>
2024-05-01 19:44:00 -0500

    proyecto-final

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+import pickle
 
 st.title('RSaid')
 datos = pd.read_csv('hprice.csv')
-print(datos.head())
+with open("model.pickle", "rb") as f:
+    model = pickle.load(f)
+st.subheader('assess')
+val_1=st.slider('Seleccione el valor',
+          datos['assess'].min(),
+          datos['assess'].max())
+st.subheader('bdrms')
+val_2=st.slider('Seleccione el valor',
+          datos['bdrms'].min(),
+          datos['bdrms'].max())
+st.subheader('lotsize')
+val_3=st.slider('Seleccione el valor',
+          datos['lotsize'].min(),
+          datos['lotsize'].max())
+st.subheader('sqrft')
+val_4=st.slider('Seleccione el valor',
+          datos['sqrft'].min(),
+          datos['sqrft'].max())
+st.subheader('colonial')
+val_5_selected=st.radio("¿Su casa es colonial?", ('Sí', 'No'))
+val_5 = 1 if val_5_selected == 'Sí' else 0
+
+valores=np.array([[val_1,val_2,val_3,val_4,val_5]])
+
+precio=model.predict(valores)
+st.write(precio)
